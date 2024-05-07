e2a9048aed80c0fe1e36ebfc38279adac24d9a69
matilda002 <matilda0817@hotmail.com>
2024-05-07 10:12:40 +0200

    added xlog

--- Backend/unit-tests/UtilsTest.cs
+++ Backend/unit-tests/UtilsTest.cs
@@ -1,12 +1,18 @@
 ﻿using Xunit;
 namespace WebApp;
 
-public class UtilsTest
+public class UtilsTest (Xlog Console)
 {
     [Fact]
     public void TestIsPasswordGoodEnough()
     {
         bool result = Utils.IsPasswordGoodEnough("Password1!");
         Assert.True(result);
+    }
+    
+    [Fact]
+    public void TestRemoveBadWords()
+    {
+        FilePath("json", "badwords.json");
     }
 }
