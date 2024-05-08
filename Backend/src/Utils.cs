c8700e8b2deb84693455b21d57507a94438abe1a
matilda002 <matilda0817@hotmail.com>
2024-05-08 09:28:32 +0200

    added mockuser template
    yet to make it work

--- Backend/src/Utils.cs
+++ Backend/src/Utils.cs
@@ -17,4 +17,28 @@ public static class Utils
         }
         return result;
     }
+
+    public static Arr CreateMockUsers() 
+    {
+            // Read all mock users from the JSON file
+            var read = FilePath(FilePath("json", "mock-users.json"));
+            Arr mockUsers = JSON.Parse(read);
+            Arr successFullyWrittenUsers = Arr();
+            foreach (var user in mockUsers)
+            {
+                var result = SQLQueryOne(
+                    @"INSERT INTO users(firstName,lastName,email,password)
+                    VALUES($firstName, $lastName, $email, $password)
+                ", user);
+                // If we get an error from the DB then we haven't added
+                // the mock users, if not we have so add to the successful list
+                if (!result.HasKey("error"))
+                {
+                    // The specification says return the user list without password
+                    user.Delete("password");
+                    successFullyWrittenUsers.Push(user);
+                }
+            }
+            return successFullyWrittenUsers;
+    }
 }
