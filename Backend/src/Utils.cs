46c5dd1f639ed1c9f4d51a0cbb59e6c8c504ab11
matilda002 <matilda0817@hotmail.com>
2024-05-14 10:29:07 +0200

    added thomas code example
    checks if password is long enough, contains no bad words and adds mockusers

--- Backend/src/Utils.cs
+++ Backend/src/Utils.cs
@@ -2,20 +2,69 @@
 
 public static class Utils
 {
-    
+    // Read all mock users from file
+    private static readonly Arr mockUsers = JSON.Parse(
+        File.ReadAllText(FilePath("json", "mock-users.json"))
+    );
+
+    // Read all bad words from file and sort from longest to shortest
+    // if we didn't sort we would often get "---ing" instead of "---" etc.
+    // (Comment out the sort - run the unit tests and see for yourself...)
+    private static readonly Arr badWords = ((Arr)JSON.Parse(
+        File.ReadAllText(FilePath("json", "bad-words.json"))
+    )).Sort((a, b) => ((string)b).Length - ((string)a).Length);
+
     public static bool IsPasswordGoodEnough(string password)
     {
-        bool result;
-        
-        if (password.Length > 6 && password.Any(char.IsLower) && password.Any(char.IsUpper) &&
-            password.Any(char.IsNumber) && password.Any(char.IsSymbol) || password.Any(char.IsPunctuation))
-        {
-            result = true;
-        }
-        else
-        {
-            result = false;
-        }
-        return result;
+        return password.Length >= 8
+            && password.Any(Char.IsDigit)
+            && password.Any(Char.IsLower)
+            && password.Any(Char.IsUpper)
+            && password.Any(x => !Char.IsLetterOrDigit(x));
     }
+
+    public static bool IsPasswordGoodEnoughRegexVersion(string password)
+    {
+        // See: https://dev.to/rasaf_ibrahim/write-regex-password-validation-like-a-pro-5175
+        var pattern = @"^(?=.*[0-9])(?=.*[a-zåäö])(?=.*[A-ZÅÄÖ])(?=.*\W).{8,}$";
+        return Regex.IsMatch(password, pattern);
+    }
+
+    public static string RemoveBadWords(string comment, string replaceWith = "---")
+    {
+        comment = " " + comment;
+        replaceWith = " " + replaceWith + "$1";
+        badWords.ForEach(bad =>
+        {
+            var pattern = @$" {bad}([\,\.\!\?\:\; ])";
+            comment = Regex.Replace(
+                comment, pattern, replaceWith, RegexOptions.IgnoreCase);
+        });
+        return comment[1..];
+    }
+
+    public static Arr CreateMockUsers()
+    {
+        Arr successFullyWrittenUsers = Arr();
+        foreach (var user in mockUsers)
+        {
+            // user.password = "12345678";
+            var result = SQLQueryOne(
+                @"INSERT INTO users(firstName,lastName,email,password)
+                VALUES($firstName, $lastName, $email, $password)
+            ", user);
+            // If we get an error from the DB then we haven't added
+            // the mock users, if not we have so add to the successful list
+            if (!result.HasKey("error"))
+            {
+                // The specification says return the user list without password
+                user.Delete("password");
+                successFullyWrittenUsers.Push(user);
+            }
+        }
+        return successFullyWrittenUsers;
+    }
+
+    // Now write the two last ones yourself!
+    // See: https://sys23m-jensen.lms.nodehill.se/uploads/videos/2021-05-18T15-38-54/sysa-23-presentation-2024-05-02-updated.html#8
 }
--- Backend/unit-tests/UtilsTest.cs
+++ Backend/unit-tests/UtilsTest.cs
@@ -1,18 +1,78 @@
-﻿using Xunit;
-namespace WebApp;
+﻿namespace WebApp;
 
-public class UtilsTest (Xlog Console)
+public class UtilsTest(Xlog Console)
 {
-    [Fact]
-    public void TestIsPasswordGoodEnough()
+    // Read all mock users from file
+    private static readonly Arr mockUsers = JSON.Parse(
+        File.ReadAllText(FilePath("json", "mock-users.json"))
+    );
+
+    [Theory]
+    [InlineData("abC9#fgh", true)]  // ok
+    [InlineData("stU5/xyz", true)]  // ok too
+    [InlineData("abC9#fg", false)]  // too short
+    [InlineData("abCd#fgh", false)] // no digit
+    [InlineData("abc9#fgh", false)] // no capital letter
+    [InlineData("abC9efgh", false)] // no special character
+    public void TestIsPasswordGoodEnough(string password, bool expected)
     {
-        bool result = Utils.IsPasswordGoodEnough("Password1!");
-        Assert.True(result);
+        Assert.Equal(expected, Utils.IsPasswordGoodEnough(password));
     }
-    
-    [Fact]
-    public void TestRemoveBadWords()
+
+    [Theory]
+    [InlineData("abC9#fgh", true)]  // ok
+    [InlineData("stU5/xyz", true)]  // ok too
+    [InlineData("abC9#fg", false)]  // too short
+    [InlineData("abCd#fgh", false)] // no digit
+    [InlineData("abc9#fgh", false)] // no capital letter
+    [InlineData("abC9efgh", false)] // no special character
+    public void TestIsPasswordGoodEnoughRegexVersion(string password, bool expected)
     {
-        FilePath("json", "badwords.json");
+        Assert.Equal(expected, Utils.IsPasswordGoodEnoughRegexVersion(password));
     }
+
+    [Theory]
+    [InlineData(
+        "---",
+        "Hello, I am going through hell. Hell is a real fucking place " +
+            "outside your goddamn comfy tortoiseshell!",
+        "Hello, I am going through ---. --- is a real --- place " +
+            "outside your --- comfy tortoiseshell!"
+    )]
+    [InlineData(
+        "---",
+        "Rhinos have a horny knob? (or what should I call it) on " +
+            "their heads. And doorknobs are damn round.",
+        "Rhinos have a --- ---? (or what should I call it) on " +
+            "their heads. And doorknobs are --- round."
+    )]
+    public void TestRemoveBadWords(string replaceWith, string original, string expected)
+    {
+        Assert.Equal(expected, Utils.RemoveBadWords(original, replaceWith));
+    }
+
+    [Fact]
+    public void TestCreateMockUsers()
+    {
+        // Get all users from the database
+        Arr usersInDb = SQLQuery("SELECT email FROM users");
+        Arr emailsInDb = usersInDb.Map(user => user.email);
+        // Only keep the mock users not already in db
+        Arr mockUsersNotInDb = mockUsers.Filter(
+            mockUser => !emailsInDb.Contains(mockUser.email)
+        );
+        // Get the result of running the method in our code
+        var result = Utils.CreateMockUsers();
+        // Assert that the CreateMockUsers only return
+        // newly created users in the db
+        Console.WriteLine($"The test expected that {mockUsersNotInDb.Length} users should be added.");
+        Console.WriteLine($"And {result.Length} users were added.");
+        Console.WriteLine("The test also asserts that the users added " +
+            "are equivalent (the same) as the expected users!");
+        Assert.Equivalent(mockUsersNotInDb, result);
+        Console.WriteLine("The test passed!");
+    }
+
+    // Now write the two last ones yourself!
+    // See: https://sys23m-jensen.lms.nodehill.se/uploads/videos/2021-05-18T15-38-54/sysa-23-presentation-2024-05-02-updated.html#8
 }
