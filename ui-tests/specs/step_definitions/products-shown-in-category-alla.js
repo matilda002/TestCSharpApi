f7cd2eee8b8e17f72926ad999ff861282275f7b6
matilda002 <matilda0817@hotmail.com>
2024-05-21 11:18:18 +0200

    ui-test for products shown in category 'Alla' done

---
+++ ui-tests/specs/step_definitions/products-shown-in-category-alla.js
@@ -0,0 +1,13 @@
+﻿import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given('that I am on the product page', () => {
+  cy.visit("/products")
+});
+
+When('I choose the category "Alla"', () => {
+    cy.get('#categories').select('Alla')
+});
+
+Then('The product {string} should be shown', (productName) => {
+    cy.get('.products').contains('.name', productName).should('be.visible')
+});
